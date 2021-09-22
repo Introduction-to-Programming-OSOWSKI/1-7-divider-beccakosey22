bf9cbe1277a4751fc73f802f90555e229b74b265
beccakosey22 <820512@mystma.org>
2021-09-22 08:49:53 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a function called divider() that returns the quotient of two given variables x and y
+def divider(x, y):
+    return x / y
+
+print(divider(4, 2))
